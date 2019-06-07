15197464742145aed2f0247e1217e5dc568ae43e
Justin Hutsell <hutselljustin@gmail.com>
2019-06-07 20:55:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,10 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name == nil 
+    if self.find_by_name(name) == nil 
+      self.create_by_name(name)
+    else
+      
       
   end
 
